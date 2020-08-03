c130df0eea5add08d43e534ed3f0d1fa821ec1f5
Condor Andrei <44895249+condoran@users.noreply.github.com>
2020-08-04 01:11:24 +0300

    First scene

---
+++ Assets/Scripts/CharacterController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Numerics;
+using UnityEngine;
+
+public class CharacterController : MonoBehaviour
+{
+    public float movementSpeed;
+
+    private Rigidbody2D body;
+    private UnityEngine.Vector2 moveVelocity;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        body = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        UnityEngine.Vector2 moveInput = new UnityEngine.Vector2(Input.GetAxis("Horizontal"), 0);
+        moveVelocity = moveInput * movementSpeed;
+    }
+
+    private void FixedUpdate()
+    {
+        body.AddForce(moveVelocity * Time.fixedDeltaTime);
+    }
+}
