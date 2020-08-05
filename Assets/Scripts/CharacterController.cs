8d5dc89d6353e315d51a681d87a8080bfadcfe4c
Condor Andrei <44895249+condoran@users.noreply.github.com>
2020-08-06 01:47:53 +0300

    platforms, throwing, terry
    Fallthrough platforms, throwing the tape upwards and added terry model

--- Assets/Scripts/CharacterController.cs
+++ Assets/Scripts/CharacterController.cs
@@ -1,21 +1,29 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
-using System.Numerics;
 using UnityEngine;
 
 public class CharacterController : MonoBehaviour
 {
     public float movementSpeed;
     public float jumpSpeed;
+    public Transform spawner;
+    public GameObject tape;
+    public float throwPower;
 
     private Rigidbody2D body;
-    private UnityEngine.Vector2 moveVelocity;
+    private Vector2 moveVelocity;
     private bool jump;
     private bool facesRight;
+    private float gravity;
+    private float waitTime = 0.1f;
+    private GameObject underPlayer;
+    private bool hasTape;
+    private Rigidbody2D tapeBody;
 
     // Start is called before the first frame update
     void Start()
     {
+        hasTape = true;
         jump = false;
         facesRight = true;
         body = GetComponent<Rigidbody2D>();
@@ -26,22 +34,62 @@ public class CharacterController : MonoBehaviour
     {
         if (Input.GetButtonDown("Jump") && !jump)
         {
-            body.AddForce(UnityEngine.Vector2.up * jumpSpeed);
-            Debug.Log(body.velocity);
+            body.AddForce(Vector2.up * jumpSpeed);
+            //Debug.Log(body.velocity);
             jump = true;
+        }
+        
+        if (Input.GetAxis("Vertical") < 0)
+        {
+            if (waitTime <= 0)
+            {
+                if (underPlayer.GetComponent<FallThroughPlatform>() != null)
+                {
+                    underPlayer.GetComponent<FallThroughPlatform>().ChangeRotation();
+                }
+            }
+            else
+            {
+                waitTime -= Time.deltaTime;
+            }
+        }
+
+        else if (Input.GetAxis("Vertical") == 0)
+        {
+            waitTime = 0.5f;
         }
         
         if (!jump)
         { 
-            UnityEngine.Vector2 moveInput = new UnityEngine.Vector2(Input.GetAxis("Horizontal"), 0);
+            Vector2 moveInput = new Vector2(Input.GetAxis("Horizontal"), 0);
             moveVelocity = moveInput * movementSpeed;
-            body.AddForce(moveVelocity * Time.fixedDeltaTime);
+            body.AddForce(moveVelocity);
         }
         else
         {
-            UnityEngine.Vector2 moveInput = new UnityEngine.Vector2(Input.GetAxis("Horizontal"), 0);
+            Vector2 moveInput = new Vector2(Input.GetAxis("Horizontal"), 0);
             moveVelocity = moveInput * movementSpeed / 4;
-            body.AddForce(moveVelocity * Time.fixedDeltaTime);
+            body.AddForce(moveVelocity);
+        }
+
+        if ((Input.GetAxis("Horizontal") < 0 && facesRight) || (Input.GetAxis("Horizontal") > 0 && !facesRight))
+        {
+            Vector3 theScale = transform.localScale;
+            theScale.x *= -1;
+            transform.localScale = theScale;
+            facesRight = !facesRight;
+        }
+
+        if (Input.GetButtonDown("Fire1"))
+        {
+            if(hasTape)
+            {
+                Throw();
+            }
+            else
+            {
+                Pickup();
+            }
         }
     }
 
@@ -50,6 +98,22 @@ public class CharacterController : MonoBehaviour
         if (collision.gameObject.tag == "Ground")
         {
             jump = false;
+            underPlayer = collision.gameObject;
         }
+    }
+
+    private void Throw()
+    {
+        tapeBody = Instantiate(tape, spawner.position, spawner.rotation).GetComponent<Rigidbody2D>();
+        if (Input.GetAxis("Vertical") > 0)
+        {
+            //Debug.Log("CPLM?");
+            tapeBody.AddForce(Vector2.up * throwPower);
+        }
+    }
+
+    private void Pickup()
+    {
+
     }
 }
---
+++ Assets/Scripts/FallThroughPlatform.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FallThroughPlatform : MonoBehaviour
+{
+    private BoxCollider2D col;
+    private float startingOffset;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        col = GetComponent<BoxCollider2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+    public void ChangeRotation()
+    {
+        StartCoroutine(RotatePlatform());
+    }
+
+    private IEnumerator RotatePlatform()
+    {
+        col.enabled = false;
+
+        yield return new WaitForSeconds(1f);
+
+        col.enabled = true;
+    }
+}
